5ea4ac87563d15e1163a1506b2068d891c9bd77f
Sir-Timz <timiokeyode@hotmail.co.uk>
2024-11-24 01:50:40 +0000

    converting to maven project and adding victory drive

--- src/main/java/Main_Game.java
+++ src/main/java/Main_Game.java
@@ -1,5 +1,7 @@
 
 public class Main_Game {
+	public static int round = 0;
+	public static int score = 0;
   public static void main(String[] args) {
 	
   }
---
+++ src/main/java/VictoryDrive.java
@@ -0,0 +1,8 @@
+public class VictoryDrive {
+	final double botSpeedValue = 0.27;
+	int botSpeed;
+	public static void main(String[]args) {
+		
+	}
+	
+}
